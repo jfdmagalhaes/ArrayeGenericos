58947924ab35b39dbf22f97e2cddf90599f7a335
Jessica Magalhaes <jfdmagalhaes@hotmail.com>
2021-04-19 19:02:15 -0300

    primeiro commit

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // açúcar sintático 
+            ContaCorrente[] contas = new ContaCorrente[]
+           {
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 4456668),
+                new ContaCorrente(874, 7781438),
+           };
+
+            for (int indice = 0; indice < contas.Length; indice++)
+            {
+                ContaCorrente contaAtual = contas[indice];
+                Console.WriteLine($"Conta {indice} {contaAtual.Numero}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
