d7a4bc8b76a8196626048413734c0f04b0648cbe
Jessica Magalhaes <jfdmagalhaes@hotmail.com>
2021-04-25 18:31:55 -0300

    Deslocando itens no array

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -15,7 +15,8 @@ namespace ByteBank.SistemaAgencia
             // açúcar sintático 
             ListaDeContaCorrente lista = new ListaDeContaCorrente();
             {
-
+                ContaCorrente contaDaJe = new ContaCorrente(12222, 222222);
+                lista.Adicionar(contaDaJe);
                 lista.Adicionar(new ContaCorrente(345, 23462));
                 lista.Adicionar(new ContaCorrente(363, 22451));
                 lista.Adicionar(new ContaCorrente(735, 23552));
@@ -25,6 +26,14 @@ namespace ByteBank.SistemaAgencia
                 lista.Adicionar(new ContaCorrente(363, 22451));
                 lista.Adicionar(new ContaCorrente(735, 23552));
                 lista.Adicionar(new ContaCorrente(735, 23552));
+
+                lista.EscreverListaNaTela();
+                lista.Remover(contaDaJe);
+
+                Console.WriteLine("Após remover a conta");
+
+                lista.EscreverListaNaTela();
+
             };
 
    
